9b4627f0202f9397b094435bae0313e193c77ba7
Jack Daniel Brody <jackdanielbrody@gmail.com>
2018-11-04 12:43:43 -0500

    Add files via upload

---
+++ CS610 Midterm/src/MyClient.java
@@ -0,0 +1,46 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+
+public class MyClient extends Applet {
+  //private int s;
+  private String color;
+  private String message;
+  
+ /* 
+  Color [] c = {
+		  Color.BLUE, Color.GREEN
+  };
+	*/
+  
+  public void init(){
+    setSize(400, 400);
+    try{
+      InetAddress host = InetAddress.getByName("localhost");
+      Socket clientSocket = new Socket(host, 16790);
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      color = in.readLine();
+      
+    
+      if (color.equalsIgnoreCase("blue")) {
+    	  setBackground(Color.BLUE);
+      }
+      else if (color.equalsIgnoreCase("green")) {
+    	  setBackground(Color.GREEN);
+      }
+    
+      message = in.readLine();
+      
+    }catch(Exception e){
+      System.out.println("Error: " + e);
+      System.exit(0);
+    }
+  }
+
+  public void paint(Graphics page){
+  
+    page.drawString(message, 20, 200);
+  }
+}
